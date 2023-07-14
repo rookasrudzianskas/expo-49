b5731267104e565bc406acc9b798b391f7a695a1
Rokas <rookas.rudzenskas@gmail.com>
2023-07-14 12:23:51 +0300

    FIX

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, ActivityIndicator } from "react-native";
+import { useEffect, useState } from "react";
+import apodsJson from "../src/data/apods.json";
+import ApodListItem from "../src/components/ApodListItem";
+import FullScreenImage from "../src/components/FullScreenImage";
+import { fetchApods } from "../src/api/apods";
+import { Apod } from "../src/types";
 
 export default function Page() {
+  const [apods, setApods] = useState<Apod[]>(apodsJson);
+  const [activePicture, setActivePicture] = useState(null);
+
+  useEffect(() => {
+    fetchApods().then(setApods);
+  }, []);
+
+  if (!apods) {
+    return <ActivityIndicator />;
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <>
+      <FlatList
+        data={apods}
+        renderItem={({ item }) => (
+          <ApodListItem
+            apod={item}
+            onImagePress={() => setActivePicture(item.url)}
+          />
+        )}
+      />
+
+      <FullScreenImage
+        url={activePicture}
+        onClose={() => setActivePicture(null)}
+      />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
